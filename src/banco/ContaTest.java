aaf7587fac13003458e270fbe92293830d0eb3ec
Pedro H. B. Cavalcante <pedrohbcavalcante@ufrn.edu.br>
2018-03-20 15:24:06 -0300

    try catch
    Signed-off-by: Pedro H. B. Cavalcante <pedrohbcavalcante@ufrn.edu.br>

--- src/banco/ContaTest.java
+++ src/banco/ContaTest.java
@@ -1,5 +1,6 @@
 package banco;
 
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Before;
@@ -26,14 +27,23 @@ class ContaTest {
 	void contaTestErroSaldoInsuficiente() throws OperacaoIllegalException {
 		c1 = new Conta(1, 0.0);
 		c2 = new Conta(2, 100);
-		c2.transferir(c2, c1, 200.0);
+		try{
+			c2.transferir(c2, c1, 200.0);
+		} catch (OperacaoIllegalException oie) {
+			System.out.println("Saldo insuficiente");
+		}
+	
 	}
 	
 	@Test
 	void contaTestErroErroAoCreditarValor() throws OperacaoIllegalException {
 		c1 = new Conta(1, 0.0);
 		c2 = new Conta(2, 100);
-		c2.transferir(c2, c1, -5);
+		try{
+			c2.transferir(c2, c1, -5);
+		} catch (OperacaoIllegalException oie) {
+			System.out.println("Erro ao creditar valor");
+		}
 	}
 
 }
